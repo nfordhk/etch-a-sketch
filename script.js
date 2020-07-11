5275bd9fa9d39753f0058224a6faca7d4262dc5b
Nick Ford <nfordhk@gmail.com>
2020-07-11 16:43:23 -0700

    clear grid function to reset the game

--- script.js
+++ script.js
@@ -7,5 +7,28 @@ function createDivs () {
         container.appendChild(div);
     }  
 }
+createDivs ()
 
-createDivs ();
+function changeDivsColor () {
+    const divColor = document.querySelectorAll('.grid-item')
+    console.log (divColor)
+    divColor.forEach (div => {
+        div.addEventListener ('mouseover', () => 
+        div.style.backgroundColor = 'black')
+        //'rgb(' + a + ',' + b + ',' + c + ')';
+    })
+}
+changeDivsColor()
+
+function clearGrid () {
+    const clearButton = document.querySelector('.clear_button')
+    const divColor = document.querySelectorAll('.grid-item')
+    
+    clearButton.addEventListener('click', () => {
+        divColor.forEach (div => {
+            div.style.backgroundColor = 'rgb(' + 242 + ',' + 252 + ',' + 245 + ')';
+        })
+    })
+}
+clearGrid()
+
